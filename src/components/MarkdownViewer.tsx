256e3b799d0ac2cb571c091371272e00b214dacb
Samuel <samuel1998.schwanzer@gmail.com>
2022-10-30 21:59:57 +0100

    Added highlighting code.

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -9,14 +9,34 @@ import "highlight.js/styles/github.css";
 import 'katex/dist/katex.min.css'
 import rehypeKatex from "rehype-katex";
 import {useAppSelector} from "../store/hooks";
+import rehypeHighlight from "rehype-highlight";
+// @ts-ignore
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 
 export const MarkdownViewer = ()=>{
     const text = useAppSelector(state=>state.commonReducer.text)
 
     return (
-        <ReactMarkdown className="grid-none border-gray-100 border-2 rounded-2xl pl-4 pt-2 pb-2 pr-4"
+        <ReactMarkdown className="grid-none border-gray-100 border-2 rounded-2xl pl-4 pt-2 pb-2 pr-4 overflow-scroll"
                        children={text}
-            remarkPlugins={[remarkMath, remarkGfm]}
+                       components={{
+                           code({node, inline, className, children, ...props}) {
+                               const match = /language-(\w+)/.exec(className || '')
+                               return !inline && match ? (
+                                   <SyntaxHighlighter
+                                       children={String(children).replace(/\n$/, '')}
+                                       language={match[1]}
+                                       PreTag="div"
+                                       {...props}
+                                   />
+                               ) : (
+                                   <code className={className} {...props}>
+                                       {children}
+                                   </code>
+                               )
+                           }
+                       }}
+                           remarkPlugins={[remarkMath, remarkGfm]}
             rehypePlugins={[rehypeKatex,rehypeRaw]}
         />)
 
