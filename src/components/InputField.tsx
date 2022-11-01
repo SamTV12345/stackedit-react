dfc10957c1d2b48b063f2c800cf12770d2708bea
Samuel <samuel1998.schwanzer@gmail.com>
2022-11-01 12:04:55 +0100

    Fixed slow typing with debouncing.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,11 +3,17 @@ import {useAppDispatch, useAppSelector} from "../store/hooks";
 import Editor from "@monaco-editor/react";
 import {useSampleFile} from "../hooks/useSampleFile";
 import {db} from "../database/Database";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {useDebounce} from "../hooks/DebounceHook";
 
 export const InputField = ()=>{
     const currentFile = useAppSelector(state=>state.commonReducer.currentFile?.content)
     const dispatch = useAppDispatch()
+    const [text, setText] = useState<string>('')
+
+    useDebounce(()=>{
+        dispatch(commonActions.setText(text))
+    },500,[text])
 
     useEffect(()=>{
     if(currentFile===undefined){
@@ -27,6 +33,13 @@ export const InputField = ()=>{
     }
     },[])
 
-    return <div>{currentFile && <Editor defaultValue={currentFile} language="markdown" options={{wordWrap:'on'}} onChange={(e)=>{dispatch(commonActions.setText(e))}} theme='light'
+    useEffect(()=>{
+        if(currentFile && text===''){
+            setText(currentFile)
+        }
+    },[currentFile])
+
+    return <div>{currentFile && <Editor defaultValue={text} language="markdown" options={{wordWrap:'on'}}
+                                        onChange={(e)=>{setText(e as string)}} theme='light'
                                         className="rounded-2xl border-gray-100 border-2 p-2 outline-0" />}</div>
 }
