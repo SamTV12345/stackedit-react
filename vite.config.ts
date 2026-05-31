73a2310181f9758b1485e7237062b00365254ed6
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2026-05-31 13:30:43 +0200

    chore: added / hosting

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude:'**/*.md',
-  base:'/stackedit-react/',
+  base:'/',
   define:{
     assetsInclude: ['**/*.gltf']
   },
